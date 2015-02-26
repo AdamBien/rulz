b1659ccdbe87476051bdd6a2f99aea411794acf8
Adam Bien <abien@adam-bien.com>
2015-02-26 08:58:15 +0100

    Deprecated import removed

--- em/src/test/java/com/airhacks/rulz/em/WorkshopTest.java
+++ em/src/test/java/com/airhacks/rulz/em/WorkshopTest.java
@@ -9,9 +9,9 @@ package com.airhacks.rulz.em;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,10 +19,9 @@ package com.airhacks.rulz.em;
  * limitations under the License.
  * #L%
  */
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Rule;
 import org.junit.Test;
 
