a3452939fe42e075e1991b4494c8ab1058c25c49
Adam Bien <abien@adam-bien.com>
2015-06-24 13:23:48 +0200

    Resolving URI from system properties implemented

--- jaxrsclient/src/main/java/com/airhacks/rulz/jaxrsclient/JAXRSClientProvider.java
+++ jaxrsclient/src/main/java/com/airhacks/rulz/jaxrsclient/JAXRSClientProvider.java
@@ -9,9 +9,9 @@ package com.airhacks.rulz.jaxrsclient;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,6 +42,10 @@ public class JAXRSClientProvider implements TestRule {
 
     public final static JAXRSClientProvider buildWithURI(String uri) {
         return new JAXRSClientProvider(uri);
+    }
+
+    public final static JAXRSClientProvider buildWithURI(String systemPropertyKey, String defaultUri) {
+        return buildWithURI(System.getProperty(systemPropertyKey, defaultUri));
     }
 
     public Client client() {
--- jaxrsclient/src/test/java/com/airhacks/rulz/jaxrsclient/JAXRSClientProviderTest.java
+++ jaxrsclient/src/test/java/com/airhacks/rulz/jaxrsclient/JAXRSClientProviderTest.java
@@ -9,9 +9,9 @@ package com.airhacks.rulz.jaxrsclient;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -49,7 +49,27 @@ public class JAXRSClientProviderTest {
 
         Response response = target.request(MediaType.TEXT_HTML).get();
         assertThat(response.getStatus(), is(200));
+    }
 
+    @Test
+    public void buildWithNonExistingSystemProperty() {
+        String defaultUri = "http://duke:42";
+        JAXRSClientProvider p = JAXRSClientProvider.buildWithURI("--", defaultUri);
+        assertNotNull(p);
+        String actual = p.target().getUri().toString();
+        assertThat(actual, is(defaultUri));
+    }
+
+    @Test
+    public void buildWithExistingSystemProperty() {
+        String expectedUri = "http://configured:21";
+        final String key = "server-uri";
+        System.setProperty(key, expectedUri);
+        String defaultUri = "http://duke:42";
+        JAXRSClientProvider p = JAXRSClientProvider.buildWithURI(key, defaultUri);
+        assertNotNull(p);
+        String actual = p.target().getUri().toString();
+        assertThat(actual, is(expectedUri));
     }
 
 }
