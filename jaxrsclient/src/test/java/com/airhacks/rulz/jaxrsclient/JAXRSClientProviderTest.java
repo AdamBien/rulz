ac7bd42efb801751ad5d394bdc883738e38c4d5b
Adam Bien <abien@adam-bien.com>
2015-08-28 11:26:20 +0200

    Additional system test added

--- jaxrsclient/src/test/java/com/airhacks/rulz/jaxrsclient/JAXRSClientProviderTest.java
+++ jaxrsclient/src/test/java/com/airhacks/rulz/jaxrsclient/JAXRSClientProviderTest.java
@@ -9,9 +9,9 @@ package com.airhacks.rulz.jaxrsclient;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ package com.airhacks.rulz.jaxrsclient;
  * limitations under the License.
  * #L%
  */
+import static com.airhacks.rulz.jaxrsclient.HttpMatchers.successful;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -49,6 +50,19 @@ public class JAXRSClientProviderTest {
 
         Response response = target.request(MediaType.TEXT_HTML).get();
         assertThat(response.getStatus(), is(200));
+    }
+
+    @Test
+    public void pingJavaAndVerifyWithMatcher() {
+        Client client = provider.client();
+        assertNotNull(client);
+        WebTarget target = provider.target();
+        assertNotNull(target);
+        assertNotNull(provider.addPath("en"));
+        assertNotNull(provider.target("http://www.google.de"));
+
+        Response response = target.request(MediaType.TEXT_HTML).get();
+        assertThat(response, is(successful()));
     }
 
     @Test
