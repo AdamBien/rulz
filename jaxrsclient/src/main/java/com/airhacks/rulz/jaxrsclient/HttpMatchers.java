ea233127992708f5a6ce55589a1ed49bb30040fb
Adam Bien <abien@adam-bien.com>
2015-10-20 13:43:54 +0200

    Internal server error matcher implemented

--- jaxrsclient/src/main/java/com/airhacks/rulz/jaxrsclient/HttpMatchers.java
+++ jaxrsclient/src/main/java/com/airhacks/rulz/jaxrsclient/HttpMatchers.java
@@ -9,9 +9,9 @@ package com.airhacks.rulz.jaxrsclient;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,6 +47,17 @@ public class HttpMatchers {
             public boolean matches(Object o) {
                 return (o instanceof Response)
                         && (((Response) o).getStatus() == Response.Status.NO_CONTENT.getStatusCode());
+            }
+        };
+    }
+
+    public static Matcher<Response> serverError() {
+        return new CustomMatcher<Response>("Internal server error") {
+
+            @Override
+            public boolean matches(Object o) {
+                return (o instanceof Response)
+                        && (((Response) o).getStatus() == Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
             }
         };
     }
