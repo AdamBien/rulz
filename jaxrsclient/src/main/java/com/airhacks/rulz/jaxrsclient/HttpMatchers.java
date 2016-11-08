5639be40d9eb94caa643e584042ae9f61d6294d0
Adam Bien <abien@adam-bien.com>
2016-11-08 16:09:57 +0100

    Additional description added

--- jaxrsclient/src/main/java/com/airhacks/rulz/jaxrsclient/HttpMatchers.java
+++ jaxrsclient/src/main/java/com/airhacks/rulz/jaxrsclient/HttpMatchers.java
@@ -9,9 +9,9 @@ package com.airhacks.rulz.jaxrsclient;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@ package com.airhacks.rulz.jaxrsclient;
  */
 import javax.ws.rs.core.Response;
 import org.hamcrest.CustomMatcher;
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 
 /**
@@ -78,6 +79,14 @@ public class HttpMatchers {
                     return false;
                 }
             }
+
+            @Override
+            public void describeMismatch(Object item, Description description) {
+                Response response = (Response) item;
+                description.appendText(response.getStatusInfo().getReasonPhrase() + " " + response.getStatus());
+            }
+
+
         };
     }
 }
